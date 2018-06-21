c5ccf08bc344a2f5311fccae68089edc8e8274ea
7D24H <dada7d24h@163.com>
2018-06-21 19:15:01 +0800

    lab4ok

---
+++ lab4/cmake-build-debug/CMakeFiles/lab4.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/dada/OSLab4/lab4/kernel/clock.c" "/home/dada/OSLab4/lab4/cmake-build-debug/CMakeFiles/lab4.dir/kernel/clock.c.o"
+  "/home/dada/OSLab4/lab4/kernel/console.c" "/home/dada/OSLab4/lab4/cmake-build-debug/CMakeFiles/lab4.dir/kernel/console.c.o"
+  "/home/dada/OSLab4/lab4/kernel/global.c" "/home/dada/OSLab4/lab4/cmake-build-debug/CMakeFiles/lab4.dir/kernel/global.c.o"
+  "/home/dada/OSLab4/lab4/kernel/i8259.c" "/home/dada/OSLab4/lab4/cmake-build-debug/CMakeFiles/lab4.dir/kernel/i8259.c.o"
+  "/home/dada/OSLab4/lab4/kernel/keyboard.c" "/home/dada/OSLab4/lab4/cmake-build-debug/CMakeFiles/lab4.dir/kernel/keyboard.c.o"
+  "/home/dada/OSLab4/lab4/kernel/main.c" "/home/dada/OSLab4/lab4/cmake-build-debug/CMakeFiles/lab4.dir/kernel/main.c.o"
+  "/home/dada/OSLab4/lab4/kernel/printf.c" "/home/dada/OSLab4/lab4/cmake-build-debug/CMakeFiles/lab4.dir/kernel/printf.c.o"
+  "/home/dada/OSLab4/lab4/kernel/proc.c" "/home/dada/OSLab4/lab4/cmake-build-debug/CMakeFiles/lab4.dir/kernel/proc.c.o"
+  "/home/dada/OSLab4/lab4/kernel/protect.c" "/home/dada/OSLab4/lab4/cmake-build-debug/CMakeFiles/lab4.dir/kernel/protect.c.o"
+  "/home/dada/OSLab4/lab4/kernel/queue.c" "/home/dada/OSLab4/lab4/cmake-build-debug/CMakeFiles/lab4.dir/kernel/queue.c.o"
+  "/home/dada/OSLab4/lab4/kernel/start.c" "/home/dada/OSLab4/lab4/cmake-build-debug/CMakeFiles/lab4.dir/kernel/start.c.o"
+  "/home/dada/OSLab4/lab4/kernel/systask.c" "/home/dada/OSLab4/lab4/cmake-build-debug/CMakeFiles/lab4.dir/kernel/systask.c.o"
+  "/home/dada/OSLab4/lab4/kernel/tty.c" "/home/dada/OSLab4/lab4/cmake-build-debug/CMakeFiles/lab4.dir/kernel/tty.c.o"
+  "/home/dada/OSLab4/lab4/kernel/vsprintf.c" "/home/dada/OSLab4/lab4/cmake-build-debug/CMakeFiles/lab4.dir/kernel/vsprintf.c.o"
+  "/home/dada/OSLab4/lab4/lib/klib.c" "/home/dada/OSLab4/lab4/cmake-build-debug/CMakeFiles/lab4.dir/lib/klib.c.o"
+  "/home/dada/OSLab4/lab4/lib/misc.c" "/home/dada/OSLab4/lab4/cmake-build-debug/CMakeFiles/lab4.dir/lib/misc.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../boot/include"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lab4/cmake-build-debug/CMakeFiles/lab4.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab4.dir/kernel/clock.c.o"
+  "CMakeFiles/lab4.dir/kernel/console.c.o"
+  "CMakeFiles/lab4.dir/kernel/global.c.o"
+  "CMakeFiles/lab4.dir/kernel/i8259.c.o"
+  "CMakeFiles/lab4.dir/kernel/keyboard.c.o"
+  "CMakeFiles/lab4.dir/kernel/main.c.o"
+  "CMakeFiles/lab4.dir/kernel/printf.c.o"
+  "CMakeFiles/lab4.dir/kernel/proc.c.o"
+  "CMakeFiles/lab4.dir/kernel/protect.c.o"
+  "CMakeFiles/lab4.dir/kernel/queue.c.o"
+  "CMakeFiles/lab4.dir/kernel/start.c.o"
+  "CMakeFiles/lab4.dir/kernel/systask.c.o"
+  "CMakeFiles/lab4.dir/kernel/tty.c.o"
+  "CMakeFiles/lab4.dir/kernel/vsprintf.c.o"
+  "CMakeFiles/lab4.dir/lib/klib.c.o"
+  "CMakeFiles/lab4.dir/lib/misc.c.o"
+  "lab4.pdb"
+  "lab4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
